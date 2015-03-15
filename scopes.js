9768b21b7b9a96e6e4da6669cc2ea04b7afdaf6c
Your Name <you@example.com>
2015-03-16 00:17:15 +0100

    scopes

---
+++ scopes.js
@@ -0,0 +1,11 @@
+demo.scopes = function() {
+  var a = 0;
+  for (var i = 0; i < 10; i++) {
+    var j = i;
+    a += i;
+  }
+  this.appendText('i, after loop: ' + i + '<br>');
+  this.appendText('j, after loop: ' + j + '<br>');
+};
+demo.scopes();
+
